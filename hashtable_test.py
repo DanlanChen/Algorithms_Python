6101cdc047c5ed61548d925cd67a45c3732cf3aa
Danlan CHEN <dchen53@trusty>
2016-06-22 00:01:10 -0400

    third

---
+++ hashtable_test.py
@@ -0,0 +1,24 @@
+from hashtable import HashTable
+H =  HashTable()
+print "success"
+H[54] = "cat"
+print"1"
+H[26] = "dog"
+print "2"
+H[93] = "lion"
+print "3"
+H[17] = "tiger"
+print "4"
+H[77] = "bird"
+print "5"
+H[31] = "cow"
+print "6"
+H[44] = "goat"
+print "7"
+H[55] = "pig"
+print "8"
+H[20] = "chicken"
+print "chicken success"
+print H.slots
+print H.data
+
